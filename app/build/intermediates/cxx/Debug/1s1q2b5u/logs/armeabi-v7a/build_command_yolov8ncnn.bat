40fdbf3f184c6b725b8e9e0e08ab6b82438e34a3
puputmgr <puputmangge123@gmail.com>
2024-06-17 12:42:24 +0700

    first commit

--- app/build/intermediates/cxx/Debug/1s1q2b5u/logs/armeabi-v7a/build_command_yolov8ncnn.bat
+++ app/build/intermediates/cxx/Debug/1s1q2b5u/logs/armeabi-v7a/build_command_yolov8ncnn.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\puput\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
   -C ^
-  "E:\\bisa-isyarat-yolov8-main (1)\\bisa-isyarat-yolov8-main\\app\\.cxx\\Debug\\1s1q2b5u\\armeabi-v7a" ^
+  "E:\\bisaisyarat\\app\\.cxx\\Debug\\1s1q2b5u\\armeabi-v7a" ^
   yolov8ncnn
--- app/src/main/java/com/tencent/yolov8ncnn/Yolov8Ncnn.java
+++ app/src/main/java/com/tencent/yolov8ncnn/Yolov8Ncnn.java
@@ -1,30 +1,57 @@
-// Tencent is pleased to support the open source community by making ncnn available.
-//
-// Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+/*
+ * Tencent is pleased to support the open source community by making ncnn available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
 package com.tencent.yolov8ncnn;
 
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.view.Surface;
 
-public class Yolov8Ncnn
-{
+public class Yolov8Ncnn {
+    // Load the native library
+    static {
+        System.loadLibrary("yolov8ncnn");
+    }
+
+    // Native methods for JNI calls
     public native boolean loadModel(AssetManager mgr, int modelid, int cpugpu);
     public native boolean openCamera(int facing);
     public native boolean closeCamera();
     public native boolean setOutputWindow(Surface surface);
+    public native String detect(Bitmap bitmap);
 
-    static {
-        System.loadLibrary("yolov8ncnn");
+    /**
+     * Method to initialize and load YOLOv8 model.
+     *
+     * @param assetManager The AssetManager to load model from assets.
+     * @param modelId      Model identifier.
+     * @param cpuGpu       Flag to specify whether to use CPU or GPU.
+     * @return True if model loaded successfully, false otherwise.
+     */
+    public boolean initializeModel(AssetManager assetManager, int modelId, int cpuGpu) {
+        return loadModel(assetManager, modelId, cpuGpu);
+    }
+
+    /**
+     * Method to perform object detection on a Bitmap.
+     *
+     * @param bitmap The input Bitmap for object detection.
+     * @return String containing the detection results.
+     */
+    public String performDetection(Bitmap bitmap) {
+        return detect(bitmap);
     }
 }
