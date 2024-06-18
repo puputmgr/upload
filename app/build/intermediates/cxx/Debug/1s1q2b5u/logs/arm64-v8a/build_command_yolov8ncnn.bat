3614499a702f3f5a9f39b5caa465a5085803b71c
radearsr <radea.surya4@gmail.com>
2024-06-19 00:06:50 +0700

    feat: image detection

--- app/build/intermediates/cxx/Debug/1s1q2b5u/logs/arm64-v8a/build_command_yolov8ncnn.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\puput\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\bisaisyarat\\app\\.cxx\\Debug\\1s1q2b5u\\arm64-v8a" ^
-  yolov8ncnn
--- app/build/intermediates/cxx/Debug/1s1q2b5u/logs/x86/build_command_yolov8ncnn.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\puput\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\bisaisyarat\\app\\.cxx\\Debug\\1s1q2b5u\\x86" ^
-  yolov8ncnn
